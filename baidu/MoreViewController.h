17cc2c0173588662aad4f80d30ef33eeb63c10c3
galebo <galebo@163.com>
2014-08-30 07:41:26 -0700

    重构

---
+++ baidu/MoreViewController.h
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.h
+//  baidu
+//
+//  Created by lee on 14-8-29.
+//  Copyright (c) 2014年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UITableViewController
+{
+    NSArray* listData;
+}
+@property(nonatomic,retain)    NSArray *listData; 
+
+
+@end
