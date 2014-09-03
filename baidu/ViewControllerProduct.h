5648f2f0beb744f033e879394326bc0b92ba47d4
galebo <galebo@163.com>
2014-09-02 22:59:27 -0700

    增加产品页面

---
+++ baidu/ViewControllerProduct.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerProduct.h
+//  baidu
+//
+//  Created by lee on 14-9-2.
+//  Copyright (c) 2014年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerProduct : UITableViewController
+
+@end
