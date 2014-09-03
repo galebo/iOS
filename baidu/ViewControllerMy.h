bbe16721fcd67c9f761687d55594456eb136dec7
galebo <galebo@163.com>
2014-09-03 16:52:52 -0700

    增加我的理财页面

---
+++ baidu/ViewControllerMy.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerMy.h
+//  baidu
+//
+//  Created by lee on 14-9-3.
+//  Copyright (c) 2014年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerMy : UIViewController
+
+@end
