ae1397f863b35b80e84da10acd9f86789fc59eaa
galebo <galebo@163.com>
2014-09-04 06:03:42 -0700

    保存

--- baidu/ViewControllerMy.m
+++ baidu/ViewControllerMy.m
@@ -26,24 +26,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+//    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"touming.png"] forBarMetrics:UIBarMetricsDefault];
+//    self.navigationController.navigationBar.barStyle = UIBarStyleBlackTranslucent;
     // Do any additional setup after loading the view.
 }
-
+- (void)viewWillAppear:(BOOL)animated{
+    self.navigationController.navigationBar.hidden = YES;
+    UIBarButtonItem *backButton=[[UIBarButtonItem alloc] init];
+    [backButton setTitle:@"返回"];
+    self.navigationItem.backBarButtonItem=backButton;
+}
+- (void)viewWillDisappear:(BOOL)animated{
+   self.navigationController.navigationBar.hidden = NO;
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
+    self.navigationController.navigationBar.hidden = NO;
+
+    NSLog(@"prepareForSegue");
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
-@end
+
+@end;
