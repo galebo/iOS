95b52d759d5b6c69d01c1474b47e1631de9a8184
galebo <galebo@163.com>
2014-09-04 18:22:07 -0700

    更改导航颜色

--- baidu/AppDelegate.m
+++ baidu/AppDelegate.m
@@ -14,10 +14,10 @@
 {
     // Override point for customization after application launch.
     [[UITabBar appearance] setTintColor:[UIColor colorWithRed:233.0/255.0 green:1.0/255.0 blue:2.0/255.0 alpha:1.0]];
-    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:233.0/255.0 green:1.0/255.0 blue:1.0/255.0 alpha:1.0]];
     
-    [[UINavigationBar appearance] setTitleTextAttributes:
-     [NSDictionary dictionaryWithObject:[UIColor whiteColor] forKey:NSForegroundColorAttributeName]];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:233.0/255.0 green:1.0/255.0 blue:1.0/255.0 alpha:1.0]];   
+    [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObject:[UIColor whiteColor] forKey:NSForegroundColorAttributeName]];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
     return YES;
 }
 							
